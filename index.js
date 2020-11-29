9b3a38313f28554390ee2b04d3179186a7f1f110
arun-upreti <upreti.arun123@gmail.com>
2020-11-29 14:28:39 +0530

    Final Software Engineering project signup/login

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-2").addEventListener("mouseover", function () { document.getElementById("btn-2").style.backgroundColor = "#4CAF50" });
+document.getElementById("btn-2").addEventListener("mouseout", function () { document.getElementById("btn-2").style.backgroundColor = "transparent" });
+document.getElementById("btn-3").addEventListener("mouseover", function () { document.getElementById("btn-3").style.color = "#ffffff" });
+document.getElementById("btn-3").addEventListener("mouseout", function () { document.getElementById("btn-3").style.color = "black" });
